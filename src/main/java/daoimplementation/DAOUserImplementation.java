aefe6d5cb7a011062aba14b6995bb4b2b2736b9c
David Parr <dvdprr6@gmail.com>
2017-10-22 12:49:49 -0400

    fixed typos

--- src/main/java/daoimplementation/DAOUserImplementation.java
+++ src/main/java/daoimplementation/DAOUserImplementation.java
@@ -11,15 +11,13 @@ import model.User;
 import sqlconnection.SQLConnection;
 
 public class DAOUserImplementation extends SQLConnection implements UserDAO{
-	private final String USER_NAME = "user_name";
-	private final String PASSWORD = "password";
 	
 	public DAOUserImplementation() {
 		super();
 	}
 	
 	public User selectUsernamePasswordByUsername(String username) {
-		String selectUserPass = "select user_name, password from Users where usern_name = ?";
+		String selectUserPass = "select user_name, user_password from Users where user_name = ?";
 		User user = new User();
 		
 		try(Connection connection = getConnection()){
@@ -27,8 +25,8 @@ public class DAOUserImplementation extends SQLConnection implements UserDAO{
 			ps.setString(1, username);
 			ResultSet rs = ps.executeQuery();
 			rs.next();
-			user.setUserName(rs.getString(USER_NAME));
-			user.setPassword(rs.getString(PASSWORD));
+			user.setUserName(rs.getString("user_name"));
+			user.setPassword(rs.getString("user_password"));
 			
 		}catch(SQLException sqle) {
 			sqle.printStackTrace();
