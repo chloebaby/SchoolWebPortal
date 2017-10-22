6a6954b6e53e8ffe3cbee0a04f14bbadc3c277ae
David Parr <dvdprr6@gmail.com>
2017-10-22 12:21:26 -0400

    authenticate user

--- src/main/java/controller/UserLoginController.java
+++ src/main/java/controller/UserLoginController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import dao.UserDAO;
 import daoimplementation.DAOUserImplementation;
+import model.User;
 
 public class UserLoginController extends HttpServlet {
 	private UserDAO daoUserImplmenetation;
@@ -20,6 +21,16 @@ public class UserLoginController extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
 		
+		User user = daoUserImplmenetation.selectUsernamePasswordByUsername(username);
+		
+		String returnUsername  = user.getUserName();
+		String returnPassword = user.getPassword();
+		
+		if(returnUsername.equals(username) && returnPassword.equals(password)) {
+			getServletContext().getRequestDispatcher("/jsp/homepage.jsp").forward(request, response);
+		}
 	}
 }
