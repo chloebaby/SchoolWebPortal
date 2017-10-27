3d6cb21bfdd561e53133ad4122968b8ac0495ffe
David Parr <dvdprr6@gmail.com>
2017-10-27 12:21:53 -0400

    save student to student service layer

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -3,19 +3,28 @@ package service;
 import java.util.List;
 
 import dao.SchoolDAO;
+import dao.StudentDAO;
 import daoimplementation.DAOStudentImplementation;
 import model.Student;
 
 public class StudentService implements StudentServiceInterface<Student> {
 	private SchoolDAO daoSchoolImplementation;
+	private StudentDAO daoStudentImplementation;
 	
 	
 	public StudentService() {
 		daoSchoolImplementation = new DAOStudentImplementation();
+		daoStudentImplementation = new DAOStudentImplementation();
 	}
 	
 	@Override
-	public List<Student> findAll(){
+	public List<Student> findAllStudents(){
 		return daoSchoolImplementation.select();
+	}
+	
+	@Override
+	public List<Student> saveStudent(Student entity){
+		daoStudentImplementation.insertStudent(entity);
+		return findAllStudents();
 	}
 }
