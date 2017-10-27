fd152cddcb7b523bd53ac50e0288e82398fd63d4
David Parr <dvdprr6@gmail.com>
2017-10-27 09:32:28 -0400

    modified to include the service layer

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -1,32 +1,29 @@
 package controller;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.StudentDAO;
-import dao.SchoolDAO;
-import daoimplementation.DAOStudentImplementation;
-import model.Student;
 
+import model.Student;
+import service.StudentService;
+import service.StudentServiceInterface;
+
+@SuppressWarnings("serial")
 public class StudentController extends HttpServlet{
-	private StudentDAO daoStudentImplementation;
-	private SchoolDAO<Student> daoSchoolImplementation;
+	private StudentServiceInterface<Student> studentService;
 	
 	public StudentController(){
 		super();
-		daoStudentImplementation = new DAOStudentImplementation();
-		daoSchoolImplementation = new DAOStudentImplementation();
+		studentService = new StudentService();
 	}
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		List<Student> allStudents = daoSchoolImplementation.select();
-		request.setAttribute("allStudents", allStudents);
+		request.setAttribute("allStudents", studentService.findAll());
 		getServletContext().getRequestDispatcher("/jsp/homepage.jsp").forward(request, response);
 		
 	}
