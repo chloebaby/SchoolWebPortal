88128f630b4fdaa7c8a1df2700e5e2a2f99d4dab
David Parr <dvdprr6@gmail.com>
2017-10-27 12:18:03 -0400

    doPost method

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -22,8 +24,32 @@ public class StudentController extends HttpServlet{
 	}
 	
 	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String option = request.getParameter("option");
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		
+		Student student = new Student();
+		student.setFirstName(firstName);
+		student.setLastName(lastName);
+		student.setEmail(email);
+		
+		List<Student> allStudents = new ArrayList<Student>();
+		
+		if(option.equalsIgnoreCase("save")) {
+			allStudents = studentService.saveStudent(student);
+		}
+		
+		request.setAttribute("allStudents", allStudents);
+		getServletContext().getRequestDispatcher("/jsp/homepage.jsp").forward(request, response);
+		
+		
+	}
+	
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		request.setAttribute("allStudents", studentService.findAll());
+		request.setAttribute("allStudents", studentService.findAllStudents());
 		getServletContext().getRequestDispatcher("/jsp/homepage.jsp").forward(request, response);
 		
 	}
