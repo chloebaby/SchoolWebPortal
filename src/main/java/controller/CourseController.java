0411382d292a3b2501194a10c97e71a440e41bec
David Parr <dvdprr6@gmail.com>
2017-10-29 11:42:23 -0400

    course controller

---
+++ src/main/java/controller/CourseController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.CourseService;
+import service.CourseServiceInterface;
+import model.Course;
+
+public class CourseController extends HttpServlet{
+	private CourseServiceInterface<Course> courseService;
+	
+	public CourseController() {
+		super();
+		courseService = new CourseService();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setAttribute("allCourses", courseService.findAllCourses());
+		//getServletContext().getRequestDispatcher("/jsp/homepage.jsp").forward(request, response);
+		getServletContext().getRequestDispatcher("/jsp/course-page.jsp").forward(request, response);
+		
+	}
+}
