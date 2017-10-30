ca5268d34344c295414765dae692a4464f07b955
David Parr <dvdprr6@gmail.com>
2017-10-30 15:37:39 -0400

    delete by id only

--- src/main/java/dao/StudentDAO.java
+++ src/main/java/dao/StudentDAO.java
@@ -4,5 +4,5 @@ import model.Student;
 
 public interface StudentDAO {
 	public void insertStudent(Student student);
-	public void deleteStudent(String email);
+	public void deleteStudent(int studentId);
 }
