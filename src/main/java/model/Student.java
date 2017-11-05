2a17fd86c56425270847a88b99bce5a4f1c5119c
David Parr <dvdprr6@gmail.com>
2017-11-05 15:54:55 -0500

    add password, user, and role variables

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -4,12 +4,64 @@ import java.sql.Date;
 
 public class Student {
 	private int studentId;
+	private int userId;
+	private int roleId;
+	private String password;
+	private String username;
+	private String rolename;
 	private String firstName;
 	private String lastName;
 	private String email;
 	private Date lastModified;
 	
 	public Student() {}
+	
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public int getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getRolename() {
+		return rolename;
+	}
+
+	public void setRolename(String rolename) {
+		this.rolename = rolename;
+	}
 	
 	public int getStudentId() {
 		return studentId;
@@ -50,7 +102,5 @@ public class Student {
 	public void setLastModified(Date lastModified) {
 		this.lastModified = lastModified;
 	}
-	
-	
 
 }
