bd4eebbccc5a718b8fa61d5614928c5288f5d3c9
David Parr <dvdprr6@gmail.com>
2017-11-06 21:07:20 -0500

    delete student by id method in interface

--- src/main/java/service/StudentServiceInterface.java
+++ src/main/java/service/StudentServiceInterface.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface StudentServiceInterface<T, U, V> {
 	public List<T> findAllStudents();
 	public void saveStudent(T entity1, U entity2, V entity3);
-	public void deleteStudentById(int studentId);
+	public void deleteStudentById(int studentId, int userId, String username);
 	public T findStudentById(int studentId);
 	public void updateStudent(T entity);
 }
