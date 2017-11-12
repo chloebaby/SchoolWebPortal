f5663d6b751cb4674bb0a0d0985837cda1026f6d
David Parr <dvdprr6@gmail.com>
2017-11-12 10:29:05 -0500

    update user method in interface

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -6,5 +6,6 @@ public interface UserDAO {
 	public void insertUser(User user);
 	public int selectUserIdByName(User user);
 	public void deleteUserById(int userId);
+	public void updateUser(User user);
 	
 }
