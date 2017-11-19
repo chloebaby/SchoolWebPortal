14f9a40ae7e631af583fdb8be553dbba586300c5
David Parr <dvdprr6@gmail.com>
2017-11-19 18:34:08 -0500

    remove generic tags and add close to interface

--- src/main/java/service/StudentServiceInterface.java
+++ src/main/java/service/StudentServiceInterface.java
@@ -2,10 +2,13 @@ package service;
 
 import java.util.List;
 
-public interface StudentServiceInterface<T, U, V> {
-	public List<T> findAllStudents();
-	public void saveStudent(T entity1, U entity2, V entity3);
-	public void deleteStudentById(int studentId, int userId, String username);
-	public T findStudentById(int studentId);
-	public void updateStudent(T entity1, U entity2, V entity3);
+import model.Student;
+
+public interface StudentServiceInterface {
+	public List<Student> findAllStudents();
+	public void close();
+	//public void saveStudent(T entity1, U entity2, V entity3);
+	//public void deleteStudentById(int studentId, int userId, String username);
+	//public T findStudentById(int studentId);
+	//public void updateStudent(T entity1, U entity2, V entity3);
 }
