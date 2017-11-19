a8f7ce4dd7d8687e2a32664b38aeb879dc78b75b
David Parr <dvdprr6@gmail.com>
2017-11-19 18:29:19 -0500

    User table

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,8 +1,22 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
 public class User {
+	
+	@Id
+	@Column(name = "user_id")
 	private int userId;
+	
+	@Column(name = "username")
 	private String username;
+	
+	@Column(name = "password")
 	private String password;
 	
 	public User() {}
