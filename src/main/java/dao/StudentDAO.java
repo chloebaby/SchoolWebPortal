e1482edc875bcf3cd925c640888cd9f7a0fc2403
David Parr <dvdprr6@gmail.com>
2017-11-19 18:28:44 -0500

    extend HibernateDAO

--- src/main/java/dao/StudentDAO.java
+++ src/main/java/dao/StudentDAO.java
@@ -1,9 +1,13 @@
 package dao;
 
+import java.util.List;
+
 import model.Student;
 
-public interface StudentDAO {
-	public void insertStudent(Student student);
-	public void deleteStudent(int studentId);
-	public void updateStudent(Student student);
+public interface StudentDAO extends HibernateDAO {
+	//public void insertStudent(Student student);
+	//public void deleteStudent(int studentId);
+	//public void updateStudent(Student student);
+	public List<Student> select();
+	//public Student selectById(int id);
 }
--- src/main/java/dao/UserRoleDAO.java
+++ src/main/java/dao/UserRoleDAO.java
@@ -2,7 +2,7 @@ package dao;
 
 import model.UserRole;
 
-public interface UserRoleDAO {
+public interface UserRoleDAO extends HibernateDAO {
 	public void insertUserRole(UserRole userRole);
 	public void deleteUserRoleByUsername(String username);
 	public void updateUserRole(UserRole userRole);
