6ebf0f0a8cdcb6e7d7d722092abce7afba36534c
David Parr <dvdprr6@gmail.com>
2017-11-19 18:29:36 -0500

    User Role table

--- src/main/java/model/UserRole.java
+++ src/main/java/model/UserRole.java
@@ -1,8 +1,21 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USERROLES")
 public class UserRole {
+	@Id
+	@Column(name = "userrole_id")
 	private int userRoleId;
+	
+	@Column(name = "username")
 	private String username;
+	
+	@Column(name = "rolename")
 	private String rolename;
 	
 	public UserRole() {}
