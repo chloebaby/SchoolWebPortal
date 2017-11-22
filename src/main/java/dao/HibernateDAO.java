f37ebbab943ab91c8ba739045ee563a25efdc14b
David Parr <dvdprr6@gmail.com>
2017-11-22 13:32:52 -0500

    not needed

--- src/main/java/dao/HibernateDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-public interface HibernateDAO {
-	public void openSession();
-	public void openTransaction();
-	public void closeSession();
-	public void closeTransaction();
-	public void close();
-}
