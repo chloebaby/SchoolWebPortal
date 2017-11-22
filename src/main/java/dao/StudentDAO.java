7ab01446d31821df553b759b3fb4bb91ee6ff8e9
David Parr <dvdprr6@gmail.com>
2017-11-22 16:35:47 -0500

    insertStudent method in StudentDAO interface

--- src/main/java/dao/StudentDAO.java
+++ src/main/java/dao/StudentDAO.java
@@ -5,7 +5,7 @@ import java.util.List;
 import model.Student;
 
 public interface StudentDAO {
-	//public void insertStudent(Student student);
+	public void insertStudent(Student student);
 	//public void deleteStudent(int studentId);
 	//public void updateStudent(Student student);
 	public List<Student> select();
