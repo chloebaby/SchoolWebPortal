64f6936cb4ea9e8b9cdf26cd11355514aa273bff
David Parr <dvdprr6@gmail.com>
2017-11-22 14:40:06 -0500

    add last modified column and define max lenth for role name column

--- src/main/java/model/Role.java
+++ src/main/java/model/Role.java
@@ -4,6 +4,7 @@ import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,10 +13,11 @@ import javax.persistence.Table;
 public class Role {
 	
 	@Id
+	@GeneratedValue
 	@Column(name = "role_id")
 	private int roleId;
 	
-	@Column(name = "role_name")
+	@Column(name = "role_name", length = 15)
 	private String rolename;
 	
 	@Column(name = "last_modified")
@@ -37,5 +39,13 @@ public class Role {
 	
 	public void setRolename(String rolename) {
 		this.rolename = rolename;
+	}
+
+	public Date getLastModified() {
+		return lastModified;
+	}
+
+	public void setLastModified(Date lastModified) {
+		this.lastModified = lastModified;
 	}
 }
