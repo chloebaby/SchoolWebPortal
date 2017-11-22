48ed2a82f8ae5d5c365ddcf749f98785fd39cf30
David Parr <dvdprr6@gmail.com>
2017-11-22 14:43:15 -0500

    auto generate id

--- src/main/java/model/UserRole.java
+++ src/main/java/model/UserRole.java
@@ -4,13 +4,16 @@ import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "USERROLES")
 public class UserRole {
+	
 	@Id
+	@GeneratedValue
 	@Column(name = "userrole_id")
 	private int userRoleId;
 	
