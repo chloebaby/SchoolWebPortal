91cfb286b21e50674c82a3256956ffcfac690d6a
David Parr <dvdprr6@gmail.com>
2017-11-22 16:05:40 -0500

    add save student method to student service interface

--- src/main/java/service/StudentServiceInterface.java
+++ src/main/java/service/StudentServiceInterface.java
@@ -6,7 +6,7 @@ import model.Student;
 
 public interface StudentServiceInterface {
 	public List<Student> findAllStudents();
-	//public void saveStudent(T entity1, U entity2, V entity3);
+	public void saveStudent(Student student);
 	//public void deleteStudentById(int studentId, int userId, String username);
 	//public T findStudentById(int studentId);
 	//public void updateStudent(T entity1, U entity2, V entity3);
