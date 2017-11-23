fbbbfd27d6ad8c2e03a6a76a3dc915d48e243c30
David Parr <dvdprr6@gmail.com>
2017-11-23 14:44:23 -0500

    user role service class

---
+++ src/main/java/service/UserRoleService.java
@@ -0,0 +1,18 @@
+package service;
+
+import dao.UserRoleDAO;
+import daoimplementation.DAOUserRoleImplementation;
+import model.UserRole;
+
+public class UserRoleService implements UserRoleServiceInterface{
+
+	private UserRoleDAO daoUserImplementation;
+	
+	public UserRoleService() {
+		daoUserImplementation = new DAOUserRoleImplementation();
+	}
+	
+	public void saveUserRole(UserRole userRole) {
+		daoUserImplementation.insertUserRole(userRole);
+	}
+}
