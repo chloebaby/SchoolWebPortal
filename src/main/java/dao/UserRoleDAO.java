ab8a36eba6a0d088a7764c9595189c1b7e8be21e
David Parr <dvdprr6@gmail.com>
2017-11-23 14:50:29 -0500

    insertUserRole method in UserRoleDAO interface

--- src/main/java/dao/UserRoleDAO.java
+++ src/main/java/dao/UserRoleDAO.java
@@ -2,8 +2,8 @@ package dao;
 
 import model.UserRole;
 
-public interface UserRoleDAO extends HibernateDAO {
+public interface UserRoleDAO {
 	public void insertUserRole(UserRole userRole);
-	public void deleteUserRoleByUsername(String username);
-	public void updateUserRole(UserRole userRole);
+	//public void deleteUserRoleByUsername(String username);
+	//public void updateUserRole(UserRole userRole);
 }
