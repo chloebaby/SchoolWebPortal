b5cf828be8e6197d629b07a19064ef5804bb053d
David Parr <dvdprr6@gmail.com>
2017-11-24 21:15:39 -0500

    findStudentById method in interface

--- src/main/java/service/StudentServiceInterface.java
+++ src/main/java/service/StudentServiceInterface.java
@@ -8,6 +8,6 @@ public interface StudentServiceInterface {
 	public List<Student> findAllStudents();
 	public void saveStudent(Student student);
 	//public void deleteStudentById(int studentId, int userId, String username);
-	//public T findStudentById(int studentId);
+	public Student findStudentById(int studentId);
 	//public void updateStudent(T entity1, U entity2, V entity3);
 }
