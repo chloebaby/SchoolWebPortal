1e7ea26f14dcfb18e78264baf441813c6bf67968
David Parr <dvdprr6@gmail.com>
2017-11-26 16:25:33 -0500

    enable update user role by username in interface

--- src/main/java/dao/UserRoleDAO.java
+++ src/main/java/dao/UserRoleDAO.java
@@ -5,5 +5,5 @@ import model.UserRole;
 public interface UserRoleDAO {
 	public void insertUserRole(UserRole userRole);
 	//public void deleteUserRoleByUsername(String username);
-	//public void updateUserRole(UserRole userRole);
+	public void updateUserRoleByUsername(UserRole userRole);
 }
