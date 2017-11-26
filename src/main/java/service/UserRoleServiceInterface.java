3725b809e2bc6f8fac5c032a3876d701a7a23999
David Parr <dvdprr6@gmail.com>
2017-11-26 18:24:18 -0500

    enable deleteUserRoleByUsername in interface

--- src/main/java/service/UserRoleServiceInterface.java
+++ src/main/java/service/UserRoleServiceInterface.java
@@ -5,4 +5,5 @@ import model.UserRole;
 public interface UserRoleServiceInterface {
 	public void saveUserRole(UserRole userRole);
 	public void updateUserRoleByUsername(UserRole userRole);
+	public void deleteUserRolebyUsername(String username);
 }
