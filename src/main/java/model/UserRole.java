f57b371f6b0a38484987deae6f5e31dd04bed12f
David Parr <dvdprr6@gmail.com>
2017-11-26 16:27:49 -0500

    library import

--- src/main/java/model/UserRole.java
+++ src/main/java/model/UserRole.java
@@ -3,10 +3,14 @@ package model;
 import java.sql.Date;
 import java.util.UUID;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
