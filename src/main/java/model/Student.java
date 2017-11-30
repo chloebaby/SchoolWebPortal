651586fd8ddd24119e43f9c7919265fdbf19ea11
David Parr <dvdprr6@gmail.com>
2017-11-29 20:17:49 -0500

    add list of results and join table

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import javax.persistence.Column;
@@ -9,6 +11,8 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.CascadeType;
@@ -43,6 +47,10 @@ public class Student {
 	
 	@Column(name = "email")
 	private String email;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name="STUDENT_RESULT", joinColumns={@JoinColumn(name="student_id")},inverseJoinColumns={@JoinColumn(name="result_id")})
+	private List<Result> listOfResults = new ArrayList<Result>();
 	
 	@Column(name = "last_modified")
 	private Date lastModified;
@@ -100,6 +108,16 @@ public class Student {
 		this.email = email;
 	}
 	
+	public List<Result> getListOfResults() {
+		return listOfResults;
+	}
+
+
+	public void setListOfResults(List<Result> listOfResults) {
+		this.listOfResults = listOfResults;
+	}
+
+
 	public Date getLastModified() {
 		return lastModified;
 	}
