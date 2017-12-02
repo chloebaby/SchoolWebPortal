bb0f93377a429b997c720f5891b475bd17ecd212
David Parr <dvdprr6@gmail.com>
2017-12-02 16:40:26 -0500

    add find the role ID in interface

--- src/main/java/dao/RoleDAO.java
+++ src/main/java/dao/RoleDAO.java
@@ -1,9 +1,11 @@
 package dao;
 
 import java.util.List;
+import java.util.UUID;
 
 import model.Role;
 
 public interface RoleDAO {
 	public List<Role> selectRoles();
+	public UUID selectUUIDByRoleName(String rolename);
 }
--- src/main/java/service/RoleServiceInterface.java
+++ src/main/java/service/RoleServiceInterface.java
@@ -1,9 +1,11 @@
 package service;
 
 import java.util.List;
+import java.util.UUID;
 
 import model.Role;
 
 public interface RoleServiceInterface {
 	public List<Role> findAllRoles();
+	public UUID findUUIDByRolename(String rolename);
 }
