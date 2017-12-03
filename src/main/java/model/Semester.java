d8f0eac818b0f7d8ec3b5602d5d329a11f394d27
David Parr <dvdprr6@gmail.com>
2017-12-03 14:30:31 -0500

    make semester an entity

--- src/main/java/model/Semester.java
+++ src/main/java/model/Semester.java
@@ -1,16 +1,47 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name = "Semesters")
 public class Semester {
-	private int semesterId;
+	
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@Type(type = "uuid-char")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "semester_id", updatable = false, nullable = false)
+	private UUID semesterId;
+	
+	@Column(name = "semester", updatable = false, nullable = false)
 	private String semester;
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinTable(name="SEMESTER_RESULTS", joinColumns={@JoinColumn(name="semester_id")},inverseJoinColumns={@JoinColumn(name="result_id")})
+	private List<Result> listOfResults = new ArrayList<Result>();
 	
 	public Semester() {}
 	
-	public int getSemesterId() {
+	public UUID getSemesterId() {
 		return semesterId;
 	}
 	
-	public void setSemesterId(int semesterId) {
+	public void setSemesterId(UUID semesterId) {
 		this.semesterId = semesterId;
 	}
 	
@@ -21,6 +52,4 @@ public class Semester {
 	public void setSemester(String semester) {
 		this.semester = semester;
 	}
-	
-	
 }
