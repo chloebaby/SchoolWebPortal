347e38813b41be9d6795595de00cd2b39bc409ac
David Parr <dvdprr6@gmail.com>
2017-12-08 22:21:53 -0500

    enabled selectUUIDBySemesterName

--- src/main/java/dao/SemesterDAO.java
+++ src/main/java/dao/SemesterDAO.java
@@ -1,9 +1,11 @@
 package dao;
 
 import java.util.List;
+import java.util.UUID;
 
 import model.Semester;
 
 public interface SemesterDAO {
 	public List<Semester> selectAllSemesters();
+	public UUID selectUUIDBySemesterName(String semester);
 }
