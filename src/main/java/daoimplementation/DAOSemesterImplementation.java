02c60dd226308a108b65b364e764f1cef8eb76e8
David Parr <dvdprr6@gmail.com>
2017-12-08 22:23:06 -0500

    implement selectUUIDBySemesterName

--- src/main/java/daoimplementation/DAOSemesterImplementation.java
+++ src/main/java/daoimplementation/DAOSemesterImplementation.java
@@ -1,6 +1,7 @@
 package daoimplementation;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.hibernate.query.Query;
 
@@ -22,5 +23,17 @@ public class DAOSemesterImplementation extends SQLConnection implements Semester
 		closeCurrentSession();
 		
 		return allSemesters;
+	}
+	
+	@Override
+	public UUID selectUUIDBySemesterName(String semester) {
+		String hql = "select semesterId from Semester where semester = :semester";
+		openCurrentSession();
+		Query query = getCurrentSession().createQuery(hql);
+		query.setParameter("semester", semester);
+		UUID semesterId = (UUID)query.uniqueResult();
+		closeCurrentSession();
+		
+		return semesterId;
 	}
 }
