ab7daa148a737ff98a894b5518da482d23ecdf9e
David Parr <dvdprr6@gmail.com>
2017-12-08 22:24:01 -0500

    enabled findUUIDBySemester in interface

--- src/main/java/service/SemesterServiceInterface.java
+++ src/main/java/service/SemesterServiceInterface.java
@@ -1,9 +1,11 @@
 package service;
 
 import java.util.List;
+import java.util.UUID;
 
 import model.Semester;
 
 public interface SemesterServiceInterface {
 	public List<Semester> findAllSemesters();
+	public UUID findUUIDBySemester(String semester);
 }
