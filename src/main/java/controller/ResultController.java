c50eaf96b88b24af23aa9eb13433d55be0be4e18
David Parr <dvdprr6@gmail.com>
2017-12-29 15:04:35 -0500

    update logic

--- src/main/java/controller/ResultController.java
+++ src/main/java/controller/ResultController.java
@@ -1,12 +1,14 @@
 package controller;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Result;
 import service.CourseService;
 import service.CourseServiceInterface;
 import service.ResultService;
@@ -29,17 +31,47 @@ public class ResultController extends HttpServlet{
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String option = request.getParameter(Constants.REQUEST_PARAMETER_OPTION);
 		
+		if(option.equalsIgnoreCase(Constants.REQUEST_ACTION_UPDATE)) {
+			int mark = Integer.valueOf(request.getParameter(Constants.REQUEST_PARAMETER_MARK));
+			UUID resultId = UUID.fromString(request.getParameter(Constants.REQUEST_PARAMETER_RESULTID));
+			
+			java.util.Date today = new java.util.Date();
+			java.sql.Date date = new java.sql.Date(today.getTime());
+			
+			Result result = resultService.findResultById(resultId);
+			
+			result.setMarks(mark);
+			result.setLastModified(date);
+			
+			resultService.updateResult(result);
+			
+		}
+		
+		request.setAttribute(Constants.REQUEST_ATTRIBUTE_ALLRESULTS, resultService.findAllResults());
+		request.setAttribute(Constants.REQUEST_ATTRIBUTE_ALLSTUDENTS, studentService.findAllStudents());
+		request.setAttribute(Constants.REQUEST_ATTRIBUTE_ALLCOURSES, courseService.findAllCourses());
+		getServletContext().getRequestDispatcher(Constants.REQUEST_DISPATCHER_RESULTPAGE).forward(request, response);
 	}
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		String action = request.getParameter(Constants.REQUEST_PARAMETER_ACTION);
 		
-		request.setAttribute(Constants.REQUEST_ATTRIBUTE_ALLRESULTS, resultService.findAllResults());
-		request.setAttribute(Constants.REQUEST_ATTRIBUTE_ALLSTUDENTS, studentService.findAllStudents());
-		request.setAttribute(Constants.REQUEST_ATTRIBUTE_ALLCOURSES, courseService.findAllCourses());
-		getServletContext().getRequestDispatcher(Constants.REQUEST_DISPATCHER_RESULTPAGE).forward(request, response);
-		
+		if(action.equalsIgnoreCase(Constants.REQUEST_ACTION_EDIT)) {
+			doEdit(request, response);
+		}else {
+			request.setAttribute(Constants.REQUEST_ATTRIBUTE_ALLRESULTS, resultService.findAllResults());
+			request.setAttribute(Constants.REQUEST_ATTRIBUTE_ALLSTUDENTS, studentService.findAllStudents());
+			request.setAttribute(Constants.REQUEST_ATTRIBUTE_ALLCOURSES, courseService.findAllCourses());
+			getServletContext().getRequestDispatcher(Constants.REQUEST_DISPATCHER_RESULTPAGE).forward(request, response);
+		}
+	}
+	
+	private void doEdit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		UUID resultId = UUID.fromString(request.getParameter(Constants.REQUEST_PARAMETER_RESULTID));
+		request.setAttribute(Constants.REQUEST_ATTRIBUTE_RESULT, resultService.findResultById(resultId));
+		getServletContext().getRequestDispatcher(Constants.REQUEST_DISPATCHER_RESULTMODIFYPAGE).forward(request, response);
 	}
 }
