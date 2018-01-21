d5619d7e3a1fa6eae6217da92f1e2f6b825aaea3
David Parr <dvdprr6@gmail.com>
2018-01-21 15:23:15 -0500

    changed methods to better represent implementation

--- src/main/java/dao/UserRoleDAO.java
+++ src/main/java/dao/UserRoleDAO.java
@@ -5,5 +5,6 @@ import model.UserRole;
 public interface UserRoleDAO {
 	public void insertUserRole(UserRole userRole);
 	public void deleteUserRoleByUsername(String username);
-	public void updateUserRoleByUsername(UserRole userRole);
+	public void updateUserRole(UserRole userRole);
+	public UserRole selectUserRoleByUsername(String username);
 }
