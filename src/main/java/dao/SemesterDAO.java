adf374b150a189f3406691abfd606dc06998e3e7
David Parr <dvdprr6@gmail.com>
2018-01-22 21:03:22 -0500

    changed list to set

--- src/main/java/dao/SemesterDAO.java
+++ src/main/java/dao/SemesterDAO.java
@@ -1,12 +1,12 @@
 package dao;
 
-import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import model.Semester;
 
 public interface SemesterDAO {
-	public List<Semester> selectAllSemesters();
+	public Set<Semester> selectAllSemesters();
 	public UUID selectUUIDBySemesterName(String semester);
 	public Semester selectSemesterByUUID(UUID semesterId);
 	public void updateSemester(Semester semester);
